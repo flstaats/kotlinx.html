5429bed6af07afcf26a37c559fd15179b5db4dc4
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2016-01-14 15:01:24 +0300

    Memory optimization, part 3: use singleton map

--- shared/src/main/kotlin/api.kt
+++ shared/src/main/kotlin/api.kt
@@ -60,24 +60,35 @@ fun attributesMapOf(key: String, value: String?): Map<String, String> = when (va
     else -> singletonMapOf(key, value)
 }
 fun attributesMapOf(vararg pairs: String?): Map<String, String> {
-    var result: LinkedHashMap<String, String>? = null
+    var result: Map<String, String>? = null
 
     for (i in 0 .. pairs.size - 1 step 2) {
         val k = pairs[i]
         val v = pairs[i + 1]
         if (k != null && v != null) {
             if (result == null) {
-                result = LinkedHashMap(pairs.size - i)
+                result = SingletonStringMap(k, v)
+            } else if (result is LinkedHashMap) {
+                result[k] = v
+            } else if (result is SingletonStringMap) {
+                val newMap = LinkedHashMap<String, String>(pairs.size - i + 1)
+                newMap.put(result.key, result.value)
+                newMap[k] = v
+
+                result = newMap
+            } else {
+                val newMap = LinkedHashMap<String, String>(pairs.size - i + result.size)
+                newMap.putAll(result)
+                newMap[k] = v
+
+                result = newMap
             }
-            result[k] = v
         }
     }
 
     return result ?: emptyMap
 }
-fun singletonMapOf(key: String, value: String): Map<String, String> = HashMap<String, String>(2).apply {
-    put(key, value)
-}
+fun singletonMapOf(key: String, value: String): Map<String, String> = SingletonStringMap(key, value)
 
 fun HTMLTag.unsafe(block: Unsafe.() -> Unit): Unit = consumer.onTagContentUnsafe(block)
 
@@ -91,4 +102,20 @@ class DefaultUnsafe : Unsafe {
     }
 
     override fun toString(): String = sb.toString()
+}
+
+private data class SingletonStringMap(override val key: String, override val value: String) : Map<String, String>, Map.Entry<String, String> {
+    override val entries: Set<Map.Entry<String, String>>
+        get() = setOf(this)
+    override val keys: Set<String>
+        get() = setOf(key)
+    override val size: Int
+        get() = 1
+    override val values: Collection<String>
+        get() = listOf(value)
+
+    override fun containsKey(key: String) = key == this.key
+    override fun containsValue(value: String) = value == this.value
+    override fun get(key: String): String? = if (key == this.key) value else null
+    override fun isEmpty() = false
 }
